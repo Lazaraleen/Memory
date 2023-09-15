13eab606fdf7c2bf15f559f22660931b8a47833e
Myriam JAYLE <myriamjayle@gmail.com>
2023-09-15 21:09:27 +0200

    jeu fonctionnel avec des numéros

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const card = document.querySelectorAll('.card');
 const start = document. querySelector('.start');
 
+let cartesRetournees = 0;
+let carte1 = null;
+let carte2 = null;
+let premierResultat = null;
+let secondResultat = null;
+let mouvements = 0;
+let succes = 0;
+
+let compteMouvements = document.getElementById('compteur_statistic');
+let compteSucces = document.getElementById('compteur_succes');
+
+let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+// mélanger les cartes
+numeros = numeros.sort(() => {return Math.random()-0.5});
+
+
 document.addEventListener('DOMContentLoaded', () => {
     startApp();
 })
@@ -21,9 +37,11 @@ function deblocCards() {
     }
 }
 
+// ******************* Chrono + message de fin chrono + refresh page **************************************
+
 function chrono() {
     deblocCards();
-    let time = 5;
+    let time = 60;
     start.disabled = true;
     start.classList.add('disabled');
     const count = setInterval(() => {
@@ -44,4 +62,59 @@ function chrono() {
             })
         }
     }, 1000);
+}
+
+// ************************************** Retourner les cartes ***********************************************************
+
+function retourner(id) {
+    cartesRetournees++;
+    if (cartesRetournees == 1) {
+        carte1 = document.getElementById(id);
+        premierResultat = numeros[id];
+        carte1.innerHTML = premierResultat;
+        // désactiver le premier bouton pour qu'on ne puisse plus le sélectionner
+        carte1.disabled = true;
+
+    } else if (cartesRetournees == 2) {
+        carte2 = document.getElementById(id);
+        secondResultat = numeros[id];
+        carte2.innerHTML = secondResultat;
+        carte2.disabled = true;
+
+        // augmenter le nombre de mouvements
+        mouvements++;
+        compteMouvements.innerHTML=mouvements;
+
+        if (premierResultat == secondResultat) {
+            // réinitialiser cartesRetournees
+            cartesRetournees = 0;
+
+            // augmenter le nombre de succès
+            succes++;
+            compteSucces.innerHTML=succes;
+
+            if (succes == 8) {
+                Swal.fire({
+                    position: 'center',
+                    icon: 'success',
+                    title: 'Tu as gagné !!!',
+                    showConfirmButton: true,
+                })
+                .then((result) => {
+                    if (result.isConfirmed) {
+                        location.reload();
+                    }
+                })
+            }
+        } else {
+            // les valeurs sont différentes, retourner les cartes et continuer à jouer
+            setTimeout(() => {
+                carte1.innerHTML=' ';
+                carte2.innerHTML=' ';
+                carte1.disabled = false;
+                carte2.disabled = false;
+                cartesRetournees = 0;
+            }, 1500);
+        }
+    }
 }
